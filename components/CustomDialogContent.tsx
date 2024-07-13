90658098d96d341b4027573082a958341a96ee5b
Ben Yip <ben_yip@126.com>
2024-07-13 15:38:54 +0800

    A little optimize

--- components/CustomDialogContent.tsx
+++ components/CustomDialogContent.tsx
@@ -3,11 +3,14 @@ import {
   DialogOverlay,
   DialogPortal,
 } from "@/components/ui/dialog";
-import React, { PropsWithChildren, useEffect, useRef, useState } from "react";
-import { motion, useDragControls } from "framer-motion";
+import React, { PropsWithChildren, useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 import { X } from "lucide-react";
 import { produce } from "immer";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/store";
+import { toggleMousePressing } from "@/store/commonSlice";
 
 interface Props {
   dialogVisible: boolean;
@@ -22,6 +25,11 @@ const CustomDialogContent: React.FC<PropsWithChildren<Props>> = ({
   dragConstraints,
 }) => {
   const [position, setPosition] = useState(INITIAL_POSITION);
+  const dispatch = useDispatch<AppDispatch>();
+  const { mousePressing } = useSelector((state: RootState) => state.common);
+
+  const onDragHandler = () => dispatch(toggleMousePressing(true));
+  const onDragEndHandler = () => dispatch(toggleMousePressing(false));
 
   useEffect(() => {
     if (!dialogVisible) {
@@ -45,17 +53,18 @@ const CustomDialogContent: React.FC<PropsWithChildren<Props>> = ({
   return (
     <DialogPortal>
       <DialogOverlay className="bg-transparent" />
-
       <motion.div
         drag
         dragConstraints={dragConstraints}
         dragTransition={{ power: 0 }}
         className={cn(
-          "fixed z-50 grid w-full max-w-lg gap-4 border bg-background p-6 shadow-lg sm:rounded-lg opacity-100",
-          (!position.left || !position.top) && "opacity-0 pointer-events-none"
+          "fixed z-50 grid w-full max-w-lg gap-4 border bg-background p-6 shadow-lg sm:rounded-lg opacity-100 cursor-grab",
+          mousePressing && "cursor-grabbing"
         )}
         id="dialogContent"
         style={position}
+        onMouseDown={onDragHandler}
+        onMouseUp={onDragEndHandler}
       >
         {children}
         <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
