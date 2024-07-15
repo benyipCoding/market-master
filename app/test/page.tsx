38c259a1d20485be9be08162da5f34a54ea25406
Ben Yip <ben_yip@126.com>
2024-07-15 22:07:57 +0800

    Some Adjustment.

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,47 +1,80 @@
 "use client";
-
-import * as React from "react";
-import { format } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
-
+import { Card, CardContent } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
 import {
   Popover,
-  PopoverContent,
   PopoverTrigger,
+  PopoverContent,
 } from "@/components/ui/popover";
+import { addDays, format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import React from "react";
+import { DateRange } from "react-day-picker";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 
-export default function DatePickerDemo() {
-  const [date, setDate] = React.useState<Date>();
+function DatePickerWithRange({
+  className,
+}: React.HTMLAttributes<HTMLDivElement>) {
+  const [date, setDate] = React.useState<DateRange | undefined>({
+    from: new Date(2022, 0, 20),
+    to: addDays(new Date(2022, 0, 20), 20),
+  });
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className={cn(
-            "w-[280px] justify-start text-left font-normal",
-            !date && "text-muted-foreground"
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
-        </Button>
-      </PopoverTrigger>
+    <div className={cn("grid gap-2", className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant={"outline"}
+            className={cn(
+              "w-[300px] justify-start text-left font-normal",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, "LLL dd, y")} -{" "}
+                  {format(date.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={setDate}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
 
-      <PopoverContent className="w-auto p-0">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          initialFocus
-          disabled={(date) =>
-            date > new Date() || date < new Date("1900-01-01")
-          }
-        />
-      </PopoverContent>
-    </Popover>
+export default function DemoDatePicker() {
+  return (
+    <Card>
+      <CardContent className="pt-6">
+        <div className="space-y-2">
+          <Label htmlFor="date" className="shrink-0">
+            Pick a date
+          </Label>
+          <DatePickerWithRange className="[&>button]:w-[260px]" />
+        </div>
+      </CardContent>
+    </Card>
   );
 }
--- components/SeriesDataForm.tsx
+++ components/SeriesDataForm.tsx
@@ -1,21 +1,12 @@
 import { cn } from "@/lib/utils";
 import { Label } from "@/components/ui/label";
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-} from "@/components/ui/popover";
-import { Calendar as CalendarIcon } from "lucide-react";
-import { Calendar } from "@/components/ui/calendar";
+import { Calendar as CalendarIcon, Minus, Plus } from "lucide-react";
 import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "@/components/ui/input";
-import { FaMinus, FaPlus } from "react-icons/fa6";
 import { CardContent } from "./ui/card";
-import { format } from "date-fns";
 import { SeriesDataFormValueType } from "./interfaces/SeriesSettings";
-
-const ICON_SIZE = 20;
+import { format } from "date-fns";
 
 const FormItems = [
   {
@@ -63,8 +54,14 @@ const SeriesDataForm = () => {
               </Label>
               {/* Value */}
               <div className="flex w-full items-center gap-3">
-                <Button type="button" className="rounded-sm" size="sm">
-                  <FaMinus size={ICON_SIZE} />
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="h-8 w-8 shrink-0 rounded-full"
+                  type="button"
+                >
+                  <Minus className="h-4 w-4" />
+                  <span className="sr-only">Decrease</span>
                 </Button>
                 <Input
                   placeholder={item.valueInputPlaceholder}
@@ -76,66 +73,30 @@ const SeriesDataForm = () => {
                     })
                   }
                 />
-                <Button type="button" className="rounded-sm" size="sm">
-                  <FaPlus size={ICON_SIZE} />
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="h-8 w-8 shrink-0 rounded-full"
+                  type="button"
+                >
+                  <Plus className="h-4 w-4" />
+                  <span className="sr-only">Increase</span>
                 </Button>
               </div>
               {/* DateTime */}
-              <div>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-full justify-start text-left font-normal active:scale-100",
-                        !formValue[item.timeProp] && "text-muted-foreground"
-                      )}
-                    >
-                      <CalendarIcon className="mr-2 h-4 w-4" />
-                      {formValue[item.timeProp] ? (
-                        formValue[item.timeProp]
-                      ) : (
-                        <span>Pick a date</span>
-                      )}
-                    </Button>
-                  </PopoverTrigger>
-
-                  <PopoverContent className="w-80 p-2 flex flex-col items-center">
-                    <div className="relative w-full">
-                      <CalendarIcon className="absolute left-2 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-                      <Input
-                        placeholder={item.timeInputPlaceholder}
-                        className="pl-8"
-                        value={formValue[item.timeProp]}
-                        onChange={(e) =>
-                          setFormValue({
-                            ...formValue,
-                            [item.timeProp]: e.target.value,
-                          })
-                        }
-                      />
-                    </div>
-                    <Calendar
-                      mode="single"
-                      selected={formValue[item.timeProp]}
-                      onSelect={(date) =>
-                        // setFormValue({ ...formValue, [item.timeProp]: date })
-                        setFormValue({
-                          ...formValue,
-                          [item.timeProp]: format(
-                            date as Date,
-                            "yyyy-MM-dd HH:mm:ss"
-                          ),
-                        })
-                      }
-                      initialFocus
-                      disabled={(date) =>
-                        date > new Date() || date < new Date("1900-01-01")
-                      }
-                      className="flex-1 flex justify-center"
-                    />
-                  </PopoverContent>
-                </Popover>
+              <div className={cn("grid gap-2")}>
+                <Button
+                  variant={"outline"}
+                  className="active:scale-100"
+                  disabled
+                >
+                  <CalendarIcon className="mr-2 h-4 w-4" />
+                  {formValue[item.timeProp] ? (
+                    format(formValue[item.timeProp], "yyyy-MM-dd HH:mm:ss")
+                  ) : (
+                    <span>Pick a date</span>
+                  )}
+                </Button>
               </div>
             </div>
           ))}
