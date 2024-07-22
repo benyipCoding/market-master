7b0651d1fd8189c111297c1784e864fc76bc41a8
Ben Yip <ben_yip@126.com>
2024-07-23 01:06:38 +0800

    ...

--- components/technicalIndex/TechnicalIndexForm.tsx
+++ components/technicalIndex/TechnicalIndexForm.tsx
@@ -1,7 +1,6 @@
 import { cn } from "@/lib/utils";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { Button } from "../ui/button";
-
 import { Input } from "@/components/ui/input";
 import { Card } from "../ui/card";
 import {
@@ -21,6 +20,19 @@ const TechnicalIndexForm: React.FC<TechnicalIndexFormProps> = ({
   const [currentTab, setCurrentTab] = useState<string>(
     SidebarNavItems[0].title
   );
+  const [searchInput, setSearchInput] = useState("");
+
+  const displayIndexList = useMemo(
+    () =>
+      SidebarNavItems.filter(
+        (item) =>
+          item.title.includes(searchInput.toUpperCase()) ||
+          item.subTitle.includes(searchInput.toUpperCase()) ||
+          item.title.includes(searchInput.toLowerCase()) ||
+          item.subTitle.includes(searchInput.toLowerCase())
+      ),
+    [searchInput]
+  );
 
   const onNavItemClick = (item: SidebarNavItemType) => {
     setCurrentTab(item.title);
@@ -28,19 +40,21 @@ const TechnicalIndexForm: React.FC<TechnicalIndexFormProps> = ({
 
   return (
     <div className="flex gap-3 max-h-[60vh] min-h-[50vh]">
-      <aside className="flex flex-col gap-2 w-fit">
+      <aside className="flex flex-col gap-2">
         <div className="relative">
           <Search className="absolute left-2.5 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
           <Input
             type="search"
             placeholder="Search index..."
             className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
           />
         </div>
 
         <ScrollArea thumbClass="dark:bg-primary-foreground">
           <div className={cn("flex flex-col space-x-0 space-y-1")}>
-            {SidebarNavItems.map((item, index) => {
+            {displayIndexList.map((item, index) => {
               const IconComponent = item.icon;
               return (
                 <Button
@@ -65,6 +79,11 @@ const TechnicalIndexForm: React.FC<TechnicalIndexFormProps> = ({
             })}
           </div>
         </ScrollArea>
+        {!displayIndexList.length && (
+          <div className="flex-1 flex justify-center items-center text-gray-400 dark:text-gray-500">
+            No index found...
+          </div>
+        )}
       </aside>
       <Card className="min-w-[25vw]">
         {currentTab === "MA" && <MASettings />}
