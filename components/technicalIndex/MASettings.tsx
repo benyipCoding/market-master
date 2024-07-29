14bd323a6b768513d6873f797308fccb20c030dc
Ben Yip <ben_yip@126.com>
2024-07-29 23:07:41 +0800

    Some change in MASettings component

--- components/technicalIndex/MASettings.tsx
+++ components/technicalIndex/MASettings.tsx
@@ -1,27 +1,72 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useMemo, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { CardContent, CardFooter } from "@/components/ui/card";
 import CommonHeader from "./CommonHeader";
 import ColorSelector from "../commonFormItem/ColorSelector";
 import LinePattern from "../commonFormItem/LinePattern";
 import NameItem from "../commonFormItem/NameItem";
-import PeriodItem, { CalculatePriceType } from "../commonFormItem/PeriodItem";
+import PeriodItem, {
+  CalculatePriceType,
+  PeriodItemRef,
+} from "../commonFormItem/PeriodItem";
 import { EMAFormValue } from "../interfaces/TechnicalIndexForm";
 import { SeriesColorType } from "@/constants/seriesOptions";
 import { DialogContext } from "@/context/Dialog";
+import { TechnicalIndexFormContext } from "./TechnicalIndexForm";
+import { AppDispatch, RootState } from "@/store";
+import { useDispatch, useSelector } from "react-redux";
+import { EmitteryContext } from "@/providers/EmitteryProvider";
+import { CustomLineSeriesType } from "@/hooks/interfaces";
+import { titleCase } from "@/utils/helpers";
+import {
+  LineSeriesPartialOptions,
+  LineWidth,
+  LineStyle,
+  DeepPartial,
+} from "lightweight-charts";
 
 const MASettings = () => {
-  const { setDialogVisible } = useContext(DialogContext);
+  const { setDialogVisible, tChartRef, setTechnicalIndicatorLines } =
+    useContext(DialogContext);
+  const { currentTab } = useContext(TechnicalIndexFormContext);
+  const mainSeries = useMemo(
+    () => tChartRef?.current?.childSeries[0],
+    [tChartRef]
+  );
+  const { selectedIndicator } = useSelector((state: RootState) => state.common);
 
   const [formValue, setFormValue] = useState<EMAFormValue>({
     id: "",
     name: "",
-    lineWidth: "2",
+    lineWidth: "1",
     lineStyle: "solid",
     period: "20",
     calculatePrice: "close",
     seriesColor: "#ffff00",
-    indicator: "MA",
+    indicator: currentTab,
+  });
+  const periodItemRef = useRef<PeriodItemRef>(null);
+
+  const { emittery } = useContext(EmitteryContext);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const generateOptions = (): LineSeriesPartialOptions => ({
+    id: selectedIndicator
+      ? selectedIndicator.options().id
+      : `${formValue.indicator}_${mainSeries?.options().id}_${Date.now()}`,
+    customTitle: formValue.name,
+    lineWidth: +formValue.lineWidth as LineWidth,
+    lineStyle: LineStyle[
+      titleCase(formValue.lineStyle) as any
+    ] as unknown as DeepPartial<LineStyle>,
+    showLabel: false,
+    color: formValue.seriesColor,
+    crosshairMarkerVisible: false,
+    customType: CustomLineSeriesType.Indicator,
+    indicator: formValue.indicator,
+    period: +formValue.period,
+    calculatePrice: formValue.calculatePrice,
+    pointMarkersVisible: false,
   });
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -40,7 +85,7 @@ const MASettings = () => {
         <CardContent>
           <div className="grid w-full items-center gap-4">
             <NameItem
-              itemLabel="Name"
+              itemLabel="Indicator Name"
               placeholder="Name of the indicator"
               inputValue={formValue.name}
               setInputValue={(name) => setFormValue({ ...formValue, name })}
@@ -56,6 +101,7 @@ const MASettings = () => {
                   calculatePrice: price as CalculatePriceType,
                 })
               }
+              ref={periodItemRef}
             />
 
             <ColorSelector
