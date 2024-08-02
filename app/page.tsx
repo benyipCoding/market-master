6e3afd98c6e901e89d1acf70442e7c4e152baf20
Ben Yip <ben_yip@126.com>
2024-08-02 22:36:43 +0800

    Add entry link in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <Link href="/playground" className="text-xl hover:underline">
+        Playground
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
