a6c9c8d393c3047287b69f88ad2ebd007fb4269d
Ben Yip <ben_yip@126.com>
2024-10-20 12:39:59 +0800

    Add middleware for authorization

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest } from "next/server";
+
+const unauthorizedRoutes = [
+  "/home",
+  "/favicon.ico",
+  "/auth/login",
+  "/playground",
+];
+export function middleware(request: NextRequest) {
+  const auth = request.cookies.get("Authentication")?.value;
+
+  if (request.nextUrl.pathname === "/") {
+    return Response.redirect(new URL("/home", request.url));
+  }
+
+  if (
+    !auth &&
+    !unauthorizedRoutes.some((route) =>
+      request.nextUrl.pathname.startsWith(route)
+    )
+  ) {
+    console.log("未经身份认证，不能访问受保护网页");
+    return Response.redirect(new URL("/auth/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
