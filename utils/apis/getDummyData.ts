008f9e0b255bedbb297a787e859baa398cf03ac5
Ben Yip <ben_yip@126.com>
2024-12-22 23:19:58 +0800

    delete getDummyData

--- utils/apis/getDummyData.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import { CandlestickData, Time } from "lightweight-charts";
-
-export const getDummyData = async () => {
-  return axios.get<CandlestickData<Time>[]>("/api/dummy");
-};
