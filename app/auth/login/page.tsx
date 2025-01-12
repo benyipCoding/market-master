b0576b8f7e598684e215e0ed0dbef54c4d5b062f
Ben Yip <ben_yip@126.com>
2025-01-13 06:07:20 +0800

    增加一个背景组件

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { Vortex } from "@/components/ui/vortex";
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    // <AuroraBackground>
+    <Vortex
+      // backgroundColor="primary"
+      rangeY={800}
+      particleCount={200}
+      //   // baseHue={120}
+      className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full relative"
+    ></Vortex>
+    // </AuroraBackground>
+  );
 };
 
 export default LoginPage;
