7dfb6d791824c40feb0fd9684ee0effdccce51eb
Ben Yip <ben_yip@126.com>
2025-01-13 13:28:30 +0800

    样式调整

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,14 +3,9 @@ import Link from "next/link";
 import React from "react";
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { Button } from "@/components/ui/button";
 
@@ -23,15 +18,17 @@ const HomeLayout = ({
     <div className="bg-black h-full">
       <h1 className="sr-only">TradingCamp</h1>
       <nav className="fixed top-0 h-12 bg-background/30 backdrop-blur z-50 w-full px-10 md:px-48 flex items-center border-b-[1px]">
-        <Image
-          src={"/tc-high-resolution-logo-transparent.png"}
-          alt="logo"
-          width={50}
-          height={50}
-          className="cursor-pointer mr-16"
-        />
+        <Link href={"/home"} className="mr-16">
+          <Image
+            src={"/tc-high-resolution-logo-transparent.png"}
+            alt="logo"
+            width={50}
+            height={50}
+          />
+        </Link>
+
         <NavigationMenu>
-          <NavigationMenuList className="flex gap-12">
+          <NavigationMenuList className="flex gap-10">
             <NavigationMenuItem>
               <Link href="/home" legacyBehavior passHref>
                 <NavigationMenuLink>Home</NavigationMenuLink>
