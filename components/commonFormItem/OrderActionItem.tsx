cf561c32e2d16df78e93e8a6311b9e6fed65dc9e
Ben Yip <ben_yip@126.com>
2025-05-31 16:41:16 +0800

    update

---
+++ components/commonFormItem/OrderActionItem.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { MiddleLabel } from "@/constants/tradingAside";
+import { Order } from "../interfaces/Playground";
+import { Checkbox } from "../ui/checkbox";
+
+interface OrderActionItemProps {
+  id: string;
+  label: string;
+  order: Order | null;
+}
+
+const OrderActionItem: React.FC<OrderActionItemProps> = ({
+  id,
+  label,
+  order,
+}) => {
+  //   useEffect(() => {
+  //     console.log({ order });
+  //   }, [order]);
+
+  return (
+    <div className="form-item">
+      <Label htmlFor={id} className="flex items-center gap-2 cursor-pointer">
+        <Checkbox
+          id={id}
+          className="border-secondary-foreground dark:data-[state=checked]:bg-white dark:data-[state=checked]:text-black"
+        />
+        {label}
+      </Label>
+      <div className="flex items-center gap-3">
+        <Input type="number" />
+        <Select
+          value={MiddleLabel[1].value}
+          // onValueChange={(width) => setLineWidth(width)}
+        >
+          <SelectTrigger>
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent position="popper">
+            {MiddleLabel.map(({ value, label }) => (
+              <SelectItem value={`${value}`} key={label}>
+                {label}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+    </div>
+  );
+};
+
+export default OrderActionItem;
--- components/playground/OrderActions.tsx
+++ components/playground/OrderActions.tsx
@@ -1,15 +1,25 @@
 import { retriveOrder } from "@/app/playground/actions/getOrders";
 import { AppDispatch, RootState } from "@/store";
-// import { setCurrentOrderId } from "@/store/dialogSlice";
-import React, { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Order } from "../interfaces/Playground";
 import { setCurrentOrderId } from "@/store/dialogSlice";
+import OrderActionItem from "../commonFormItem/OrderActionItem";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
 
 const OrderActions = () => {
   const { currentOrderId } = useSelector((state: RootState) => state.dialog);
   const dispatch = useDispatch<AppDispatch>();
   const [currentOrder, setCurrentOrder] = useState<Order | null>(null);
+  const { currentCandle } = useSelector((state: RootState) => state.fetchData);
 
   useEffect(() => {
     if (!currentOrderId) return;
@@ -24,7 +34,36 @@ const OrderActions = () => {
     };
   }, []);
 
-  return <div>{currentOrder?.opening_price}</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <Table>
+        <TableCaption>Order information.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Side</TableHead>
+            <TableHead>Quantity</TableHead>
+            <TableHead>Opening</TableHead>
+            <TableHead className="text-right">Current</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <OrderActionItem label="Stop Loss" order={currentOrder} id="stoploss" />
+      <OrderActionItem
+        label="Take Profit"
+        order={currentOrder}
+        id="takeprofit"
+      />
+    </div>
+  );
 };
 
 export default OrderActions;
